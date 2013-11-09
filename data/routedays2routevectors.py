4120ecccb2b2183691f90da48105fb06f9342ee3
A. Conrad Nied <anied@cs.washington.edu>
2013-11-09 12:50:36 -0800

    Fixed indentation

--- data/routedays2routevectors.py
+++ data/routedays2routevectors.py
@@ -2,11 +2,11 @@ import numpy as np
 
 def main():
 	directory = "/projects/onebusaway/BakerNiedMLProject/data"
-    serviceName = "intercitytransit"
-    routeName = "route13"
-    filespec = "{}/{}_{}_*.txt".format(directory, serviceName, routeName);
-    for files in os.listdir(filespec):
-    	print files
+	serviceName = "intercitytransit"
+	routeName = "route13"
+	filespec = "{}/{}_{}_*.txt".format(directory, serviceName, routeName);
+	for files in os.listdir(filespec):
+		print files
 
 if __name__ == '__main__':
-  main()
+	main()
